02ed119f2c0fa8035a0d8a1538828aa060711dde
Suyash Sunil Patalbansi <89703368+Suyash03022003@users.noreply.github.com>
2024-03-25 21:49:12 +0530

    Refactored the code base

--- src/data/SkillsData.js
+++ src/data/SkillsData.js
@@ -1,125 +1,165 @@
-import { JavascriptOriginal, TypescriptPlain, JavaOriginalWordmark, COriginal, CplusplusOriginal, PythonOriginal, Html5Original, Css3Original, ReactOriginal, NextjsOriginal, JqueryOriginal, ReduxOriginal, TailwindcssPlain, SassOriginal, BootstrapPlain, NodejsOriginal, ExpressOriginal, PhpPlain, MongodbPlain, MysqlPlainWordmark, OracleOriginal, GitOriginal, GithubOriginal } from 'devicons-react'
-
-export const SkillsData = [
+const SkillsData = [
     {
+        id: 1,
         name: 'JavaScript',
-        type: 'Programming Languages', 
-        svg: <JavascriptOriginal size='70' />
+        type: 'Programming Languages',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/768px-JavaScript-logo.png',
+        designation: 'console.log("Hello")'
     },
     {
-        name: 'Typescript', 
-        type: 'Programming Languages', 
-        svg: <TypescriptPlain size='70' />
+        id: 2,
+        name: 'Typescript',
+        type: 'Programming Languages',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png',
+        designation: 'console.log("Hello")'
     },
     {
-        name: 'Java', 
-        type: 'Programming Languages', 
-        svg: <JavaOriginalWordmark size='70' />
+        id: 3,
+        name: 'Java',
+        type: 'Programming Languages',
+        image: 'https://static-00.iconduck.com/assets.00/java-icon-756x1024-si1cd6qx.png',
+        designation: 'System.out.println("Hello")'
     },
     {
-        name: 'C', 
-        type: 'Programming Languages', 
-        svg: <COriginal size='70' />
+        id: 4,
+        name: 'C',
+        type: 'Programming Languages',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png',
+        designation: 'printf("Hello")'
     },
     {
-        name: 'C++', 
-        type: 'Programming Languages', 
-        svg: <CplusplusOriginal size='70' />
-    },
-
-    {
-        name: 'Python', 
-        type: 'Programming Languages', 
-        svg: <PythonOriginal size='70' />
+        id: 5,
+        name: 'C++',
+        type: 'Programming Languages',
+        image: 'https://raw.githubusercontent.com/Benio101/cpp-logo/master/cpp_logo.png',
+        designation: 'cout << "Hello";'
     },
     {
-        name: 'HTML5', 
-        type: 'Front-End Technologies', 
-        svg: <Html5Original size='70' />
+        id: 6,
+        name: 'Python',
+        type: 'Programming Languages',
+        image: 'https://cdn.iconscout.com/icon/free/png-256/free-python-3628999-3030224.png?f=webp',
+        designation: 'print("Hello")'
     },
     {
-        name: 'CSS3', 
-        type: 'Front-End Technologies', 
-        svg: <Css3Original size='70' />
+        id: 7,
+        name: 'HTML5',
+        type: 'Front-End Technologies',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkYjPO7FLc0ZS1qptK0lBtACZChBRH-D72nJI3UoX67w&s',
+        designation: '<h1>Hello</h1>'
     },
     {
-        name: 'React.js', 
-        type: 'Front-End Technologies', 
-        svg: <ReactOriginal size='70' />
+        id: 8,
+        name: 'CSS3',
+        type: 'Front-End Technologies',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/2048px-CSS3_logo.svg.png',
+        designation: 'h1{ color:red; }'
     },
     {
-        name: 'Next.js', 
-        type: 'Front-End Technologies', 
-        svg: <NextjsOriginal size='70' />
+        id: 9,
+        name: 'React.js',
+        type: 'Front-End Technologies',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
+        designation: '<h1>Hello</h1>'
     },
     {
-        name: 'HTML5', 
-        type: 'Front-End Technologies', 
-        svg: <Html5Original size='70' />
+        id: 10,
+        name: 'Next.js',
+        type: 'Front-End Technologies',
+        image: 'https://cdn.worldvectorlogo.com/logos/next-js.svg',
+        designation: '<h1>Hello</h1>'
     },
     {
-        name: 'JQuery', 
-        type: 'Front-End Technologies', 
-        svg: <JqueryOriginal size='70' />
+        id: 11,
+        name: 'JQuery',
+        type: 'Front-End Technologies',
+        image: 'https://cdn.worldvectorlogo.com/logos/jquery-4.svg',
+        designation: '<h1>Hello</h1>'
     },
     {
-        name: 'Tailwind CSS', 
-        type: 'Front-End Technologies', 
-        svg: <TailwindcssPlain size='70' />
+        id: 12,
+        name: 'Tailwind CSS',
+        type: 'Front-End Technologies',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX9fZzRj7BuQAtuf6RSuqIjWEaai2Vl7sFq2Y6tKq5hA&s',
+        designation: '<h1>Hello</h1>'
     },
     {
-        name: 'SASS', 
-        type: 'Front-End Technologies', 
-        svg: <SassOriginal size='70' />
+        id: 13,
+        name: 'SASS',
+        type: 'Front-End Technologies',
+        image: 'https://sass-lang.com/assets/img/styleguide/seal-color.png',
+        designation: '<h1>Hello</h1>'
     },
     {
-        name: 'Bootstrap', 
-        type: 'Front-End Technologies', 
-        svg: <BootstrapPlain size='70' />
+        id: 14,
+        name: 'Bootstrap',
+        type: 'Front-End Technologies',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png',
+        designation: '<h1>Hello</h1>'
     },
     {
-        name: 'Redux', 
-        type: 'Front-End Technologies', 
-        svg: <ReduxOriginal size='70' />
+        id: 15,
+        name: 'Redux',
+        type: 'Front-End Technologies',
+        image: 'https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png',
+        designation: '<h1>Hello</h1>'
     },
     {
-        name: 'Node.js', 
-        type: 'Back-End Technologies', 
-        svg: <NodejsOriginal size='70' />
+        id: 16,
+        name: 'Node.js',
+        type: 'Back-End Technologies',
+        image: 'https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png',
+        designation: 'console.log("Hello")'
     },
     {
-        name: 'Express.js', 
-        type: 'Back-End Technologies', 
-        svg: <ExpressOriginal size='70' />
+        id: 17,
+        name: 'Express.js',
+        type: 'Back-End Technologies',
+        image: 'https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png',
+        designation: 'console.log("Hello")'
     },
     {
-        name: 'PHP', 
-        type: 'Back-End Technologies', 
-        svg: <PhpPlain size='70' />
+        id: 18,
+        name: 'PHP',
+        type: 'Back-End Technologies',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Php_logo.svg/2560px-Php_logo.svg.png',
+        designation: 'echo "Hello";'
     },
     {
-        name: 'MongoDB', 
-        type: 'Databases', 
-        svg: <MongodbPlain size='70' />
+        id: 19,
+        name: 'MongoDB',
+        type: 'Databases',
+        image: 'https://1000logos.net/wp-content/uploads/2020/08/MongoDB-Emblem.jpg',
+        designation: 'db.collection.insertOne({"hello": "world"})'
     },
     {
-        name: 'MySQL', 
-        type: 'Databases', 
-        svg: <MysqlPlainWordmark size='70' />
+        id: 20,
+        name: 'MySQL',
+        type: 'Databases',
+        image: 'https://logowik.com/content/uploads/images/mysql8604.logowik.com.webp',
+        designation: 'SELECT "Hello, world!";'
     },
     {
-        name: 'Oracle', 
-        type: 'Databases', 
-        svg: <OracleOriginal size='70' />
+        id: 21,
+        name: 'Oracle',
+        type: 'Databases',
+        image: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/oracle_logo_icon_168918.png',
+        designation: "dbms_output.put_line(''Hello'');"
     },
     {
-        name: 'Git', 
-        type: 'Version Controls', 
-        svg: <GitOriginal size='70' />
+        id: 22,
+        name: 'Git',
+        type: 'Version Controls',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png',
+        designation: 'echo "Hello";'
     },
     {
-        name: 'GitHub', 
-        type: 'Version Controls', 
-        svg: <GithubOriginal size='70' />
+        id: 23,
+        name: 'GitHub',
+        type: 'Version Controls',
+        image: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+        designation: 'echo "Hello";'
     },
 ];
+
+export default SkillsData
